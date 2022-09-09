b50d9a91f8e92c282cdbbd8c792d33e251216ed1
victor willian <victorwillian1012@gmail.com>
2022-09-08 21:33:47 -0300

    ✨ feat(valueObjects):
    Add crypto to password

--- src/entities/UserEntity.ts
+++ src/entities/UserEntity.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto"
+
 export class Email {
     private _email: string
 
@@ -23,34 +25,50 @@ export class Email {
     }
 }
 
+export interface IPasswordProps {
+    password: string
+    salt: string
+}
+
 export class Password {
     private _password: string
+    private _salt: string
 
     public get value(): string {
         return this._password
     }
 
-    private constructor(password: string) {
-        this._password = this.encryptPassword(password)
+    private constructor(props: IPasswordProps) {
+        this._salt = props.salt
+            ? props.salt
+            : crypto.randomBytes(16).toString("hex")
+        this._password = this.encryptPassword(props.password)
     }
 
-    public static create(password: string): Password {
+    public static create(passwordProps: IPasswordProps): Password {
         // validate password
-        if (!password) throw new Error("Password is required")
-        if (password.length > 255) throw new Error("Password is too long")
-        if (password.length < 3) throw new Error("Password is too short")
-        const passwordObj = new Password(password)
+        if (!passwordProps.password) throw new Error("Password is required")
+        if (passwordProps.password.length > 255)
+            throw new Error("Password is too long")
+        if (passwordProps.password.length < 3)
+            throw new Error("Password is too short")
+        const passwordObj = new Password(passwordProps)
         return passwordObj
     }
 
     private encryptPassword(password: string): string {
-        // TODO encrypt password
-        return password
+        const hashedPassword = crypto
+            .pbkdf2Sync(password, this._salt, 1000, 64, `sha512`)
+            .toString(`hex`)
+
+        return hashedPassword
     }
 
-    private comparePassword(password: string): boolean {
-        // TODO compare password
-        return false
+    public comparePassword(password: string): boolean {
+        const hash = crypto
+            .pbkdf2Sync(password, this._salt, 1000, 64, `sha512`)
+            .toString(`hex`)
+        return this._password === hash
     }
 }
 
