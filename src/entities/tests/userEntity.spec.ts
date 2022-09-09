92dc9c311513891dd9e5de79bd36355c4690a94e
victor willian <victorwillian1012@gmail.com>
2022-09-08 21:34:14 -0300

    🧪 test(userEntity):
    Add test to password crypto

--- src/entities/tests/userEntity.spec.ts
+++ src/entities/tests/userEntity.spec.ts
@@ -8,7 +8,9 @@ describe("UserEntity", () => {
     })
 
     it("should throw error without password", () => {
-        expect(() => Password.create("")).toThrowError("Password is required")
+        expect(() => Password.create({ password: "", salt: "" })).toThrowError(
+            "Password is required"
+        )
     })
 
     it("should throw error with invalid email length", () => {
@@ -22,13 +24,13 @@ describe("UserEntity", () => {
     })
 
     it("should throw error with password password length", () => {
-        expect(() => Password.create("a".repeat(256))).toThrowError(
-            "Password is too long"
-        )
+        expect(() =>
+            Password.create({ password: "a".repeat(256), salt: "" })
+        ).toThrowError("Password is too long")
 
-        expect(() => Password.create("a".repeat(2))).toThrowError(
-            "Password is too short"
-        )
+        expect(() =>
+            Password.create({ password: "a".repeat(2), salt: "" })
+        ).toThrowError("Password is too short")
     })
 
     it("should throw error with invalid email", () => {
@@ -50,7 +52,7 @@ describe("UserEntity", () => {
             id: "id",
             name: "",
             email: Email.create("jonhdoe@gmail.com"),
-            password: Password.create("12345"),
+            password: Password.create({ password: "12345", salt: "" }),
         }
 
         expect(() => UserEntity.create(userProps)).toThrowError(
@@ -60,17 +62,61 @@ describe("UserEntity", () => {
 
     it("should create a user", () => {
         const userProps: IUserEntityProps = {
-            id: "id",
-            name: "name",
+            id: "8432742374823",
+            name: "john doe",
             email: Email.create("jonhdoe@gmail.com"),
-            password: Password.create("12345"),
+            password: Password.create({ password: "12345", salt: "" }),
         }
 
         const user = UserEntity.create(userProps)
 
-        expect(user.id).toBe(userProps.id)
-        expect(user.name).toBe(userProps.name)
-        expect(user.email.value).toBe(userProps.email.value)
-        expect(user.password.value).toBe(userProps.password.value)
+        expect(user.id).toBe("8432742374823")
+        expect(user.name).toBe("john doe")
+        expect(user.email.value).toBe("jonhdoe@gmail.com")
+        expect(user.password.value).not.toBe("12345")
+    })
+
+    it("Should create user and crypto password", () => {
+        const userProps: IUserEntityProps = {
+            id: "8432742374823",
+            name: "john doe",
+            email: Email.create("johndoe@gmail.com"),
+            password: Password.create({ password: "12345", salt: "" }),
+        }
+
+        const user = UserEntity.create(userProps)
+
+        expect(user.password.value).not.toBe("12345")
+    })
+
+    it("Should compare password", () => {
+        const userProps: IUserEntityProps = {
+            id: "8432742374823",
+            name: "john doe",
+            email: Email.create("johndoe@gmail.com"),
+            password: Password.create({ password: "12345", salt: "" }),
+        }
+
+        const user = UserEntity.create(userProps)
+
+        expect(user.password.comparePassword("12345")).toBe(true)
+
+        expect(user.password.comparePassword("123456")).toBe(false)
+    })
+
+    it("Should return true with salt", () => {
+        const userProps: IUserEntityProps = {
+            id: "8432742374823",
+            name: "john doe",
+            email: Email.create("johndoe@gmail.com"),
+            password: Password.create({
+                password: "12345",
+                salt: "4666d4544c08bcbd6306ef4b842b1b3f",
+            }),
+        }
+
+        const user = UserEntity.create(userProps)
+
+        expect(user.password.comparePassword("12345")).toBe(true)
     })
 })
